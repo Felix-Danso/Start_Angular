78013be1eda7c0421fd732815a7efb336536b64a
Felix-Danso <117346298+Felix-Danso@users.noreply.github.com>
2023-09-08 11:50:46 +0000

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { containerComponent } from './Container/container.component';
+import { NavComponent } from './nav/nav.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    containerComponent,
+    NavComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
