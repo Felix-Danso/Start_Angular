01a424095367df78c632a0c5d0844b5ae0f22a24
Felix-Danso <117346298+Felix-Danso@users.noreply.github.com>
2023-09-13 09:03:24 +0000

    leasson on 13th october 2023

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-nav',
+  // selector: 'app-nav', html tag
+  // selector: '[app-nav]', html attribute
+  // css class
+  selector: '.app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
